c44b5484053a1dd16e6eaa94d0d40ef252079658
Andrew Pecha <andrew.pecha@gmail.com>
2017-05-04 15:26:20 -0500

    done through 207

--- SportsStore/src/SportsStore/Startup.cs
+++ SportsStore/src/SportsStore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SportsStore.Models;
 
 namespace SportsStore
 {
@@ -16,6 +17,8 @@ namespace SportsStore
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,14 +26,13 @@ namespace SportsStore
         {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default", 
+                    template: "{controller=Product}/{action-List}/{id?}");
             });
         }
     }
